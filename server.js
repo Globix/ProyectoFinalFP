031db943ee230469e94c189c4b8d5ac7ef7e754d
Aitor Lepe <globix28@gmail.com>
2017-06-06 23:38:36 +0200

    Reorganizacion config

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
 // Get secrets from server environment
 var botConnectorOptions = {
     appId: "6439a638-2be2-4a01-b327-ba29b08db5c5",
@@ -11,24 +17,22 @@ var botConnectorOptions = {
 var connector = new builder.ChatConnector(botConnectorOptions);
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
-
-// Setup Restify Server
-var server = restify.createServer();
-
-// Handle Bot Framework messages
+// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
-// Serve a static web page
+// Serve a static web page para saber si el bot esta bien desplegado
 server.get(/.*/, restify.serveStatic({
     'directory': '.',
     'default': 'index.html'
 }));
 
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: '%s'. Try asking for 'help' or say 'goodbye' to quit", session.message.text);
+});
+
+bot.dialog('/', function (session) {
+
+    //respond with user's message
+    session.send("You said " + session.message.text);
 });
