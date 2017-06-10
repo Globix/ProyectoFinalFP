7f1f1661ae13c114b80f902f28b19fa1c8e9ebb7
Aitor Lepe <globix28@gmail.com>
2017-06-10 02:18:39 +0200

    Arreglo errores built

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // Get secrets from server environment
 var botConnectorOptions = {
     appId: "6439a638-2be2-4a01-b327-ba29b08db5c5",
-    appPassword: "zEHyAD8CtipJpER92CukqXt"
+    appPassword: "dyFQFrDUuZt4saNd44ekvGT"
     // appId: process.env.MICROSOFT_APP_ID,
     // appPassword: process.env.MICROSOFT_APP_PASSWORD
 };
